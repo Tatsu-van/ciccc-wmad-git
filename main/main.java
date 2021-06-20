e6c9d1a1428cef43a03df22558396bbac16996b4
Tatsu-van <tatsuma.van@gmail.com>
2021-06-20 14:42:50 -0700

    added some method

---
+++ main/main.java
@@ -0,0 +1,17 @@
+public class Main{
+  public static void main(String[] args){
+
+  }
+
+  public static String[] convetToBin(String mail){
+
+  }
+
+  public static int convertToDigit(String[] bin){
+
+  }
+
+  public static String getPassword(String address, int key){
+
+  }
+}
