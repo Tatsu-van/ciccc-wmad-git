8df4e65b40fe947187dc2b0b4003ddb194f373d1
Tatsu-van <tatsuma.van@gmail.com>
2021-06-20 14:45:29 -0700

    coded some inside of the main method

--- main/main.java
+++ main/main.java
@@ -1,6 +1,13 @@
 public class Main{
   public static void main(String[] args){
+    String myMailAddress = "tatsumasato@ciccc.tech";
+    String[] myBin = convertToBin(myMailAddress);
+    int passWord = convertToDigit(myBin);
+    System.out.println(passWord);
+    String cipherText = getPassword(myMailAddress, passWord);
+    System.out.println("Your password is " + passWord);
 
+    System.out.println(cipherText);
   }
 
   public static String[] convetToBin(String mail){
