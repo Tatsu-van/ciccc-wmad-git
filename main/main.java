3139ccd57c16f8a38739915e362f54f0f880c32e
Tatsu-van <tatsuma.van@gmail.com>
2021-06-20 14:49:20 -0700

    edited body contents of the convertToBin

--- main/main.java
+++ main/main.java
@@ -11,7 +11,39 @@ public class Main{
   }
 
   public static String[] convetToBin(String mail){
+    int n = mail.length();
+        char[] charArray = new char[n];
+        int[] arrayOfNum = new int[n];
+        int currentNum;
+        char c;
+        String  c1;
+        int newC;
+        String[] binArray = new String[n];
 
+        for (int i = 0; i < n; i++){
+            charArray[i] = mail.charAt(i);
+//            System.out.println(charArray[i]);
+
+        }
+
+        for (int j = 0; j < n; j++){
+            c = charArray[j];
+            System.out.println(c);
+//            System.out.println((int)c);
+            newC =  c;
+            arrayOfNum[j] =  newC;
+//            System.out.println("a" + arrayOfNum);
+        }
+
+        for (int k = 0; k < n; k++){
+            c1 = String.valueOf(arrayOfNum[k]);
+//            System.out.println("c1 " + c1);
+            currentNum = Integer.parseInt(c1);
+            binArray[k] = Integer.toBinaryString(currentNum);
+        }
+
+
+        return binArray;
   }
 
   public static int convertToDigit(String[] bin){
